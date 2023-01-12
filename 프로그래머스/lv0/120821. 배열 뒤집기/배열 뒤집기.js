70a140dfb6bdc5e11ed58ac1b881ad9b21a8b781
MIN YOUNG <118191378+FROM-minyoung@users.noreply.github.com>
2023-01-13 00:15:14 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.js
@@ -0,0 +1,12 @@
+function solution(num_list) {
+    // const twistList = []
+    // num_list.forEach((number) => {
+    //    twistList.unshift(number)
+    // })
+    // return twistList;
+    return num_list.reverse()
+
+    /* 혹은 
+    return num_list.reverse()
+    */
+}
