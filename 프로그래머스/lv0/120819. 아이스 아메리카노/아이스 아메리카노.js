71bda83a7cfab092f9bb9964a554a8d315145401
MIN YOUNG <118191378+FROM-minyoung@users.noreply.github.com>
2023-01-15 00:26:10 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.js
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.js
@@ -1,4 +1,4 @@
 function solution(money) {
-    const AMERICANO = 5500
-    return [parseInt(money / AMERICANO), money - (parseInt(money / AMERICANO) * 5500 )]
+    var answer = [];
+    return answer;
 }
