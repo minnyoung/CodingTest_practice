fed563e9b7cb8d70f30a61e15722dafda2527d79
MIN YOUNG <118191378+FROM-minyoung@users.noreply.github.com>
2023-01-28 00:03:04 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,9 +1,9 @@
 function solution(s) {
     const words = s.split(" ")
     return words.map((word) => {
-            if(word && /[a-z]/i.test(word[0])) {
+            if(word && /[a-z]/g.test(word[0])) {
               return [...word][0].toUpperCase() + word.slice(1).toLowerCase()
-        } return word && word[0] + word.slice(1).toLowerCase()
+        } return word
     }).join(" ")
 
 }
